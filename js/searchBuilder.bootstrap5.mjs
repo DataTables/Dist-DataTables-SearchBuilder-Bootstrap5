276951bcd2fd16db7d8caf8df9de2212c0da16af
dtbuild <dtbuild@rigil>
2026-05-07 14:00:59 +0000

    ae192b3a73e083969c7463e28f5904dc8930608f
    Sync to source repo @363f53b76e0441dbc1b2720ebe6f90cf3d008b35

--- js/searchBuilder.bootstrap5.mjs
+++ js/searchBuilder.bootstrap5.mjs
@@ -1,27 +1,23 @@
-/*! Bootstrap 5 ui integration for DataTables' SearchBuilder
- * © SpryMedia Ltd - datatables.net/license
+/*! SearchBuilder Bootstrap 5 styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs5';
 import SearchBuilder from 'datatables.net-searchbuilder';
 
-// Allow reassignment of the $ variable
-let $ = jQuery;
-
-$.extend(true, DataTable.SearchBuilder.classes, {
+Object.assign(DataTable.SearchBuilder.classes, {
     clearAll: 'btn btn-secondary dtsb-clearAll'
 });
-$.extend(true, DataTable.Group.classes, {
+Object.assign(DataTable.Group.classes, {
     add: 'btn btn-secondary dtsb-add',
     clearGroup: 'btn btn-secondary dtsb-clearGroup',
     logic: 'btn btn-secondary dtsb-logic',
     search: 'btn btn-secondary dtsb-search'
 });
-$.extend(true, DataTable.Criteria.classes, {
+Object.assign(DataTable.Criteria.classes, {
     condition: 'form-select dtsb-condition',
     data: 'dtsb-data form-select',
-    "delete": 'btn btn-secondary dtsb-delete',
+    delete: 'btn btn-secondary dtsb-delete',
     input: 'form-control dtsb-input',
     left: 'btn btn-secondary dtsb-left',
     right: 'btn btn-secondary dtsb-right',
@@ -31,3 +27,4 @@ $.extend(true, DataTable.Criteria.classes, {
 
 
 export default DataTable;
+
